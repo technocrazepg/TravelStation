5b7a64bdf86946431508db6a1569d54c1d47576f
pranav <pranavgupta907@gmail.com>
2022-02-06 21:27:31 +0530

    s

--- packages.php
+++ packages.php
@@ -1,4 +1,6 @@
-<section class="packages" id="packages">
+<section class="" id="packages">
+            <h1 style="text-align: center;margin-top: 2%;font-size: 2em"><b>OUR BEST PACKAGES</b></h1>
+            <div class="packages">
             <div class="pkgitem">
                 <img src="images/packages/pkg1.jpg" alt="pkg1" />
                 <br>
@@ -6,7 +8,7 @@
                 <br>
                 <p>5 days-6 nights<br><del>$2300 /-</del> $2000 /-</p>
                 <br>
-                <button>Plan</button>
+                <button><a href="sample.pdf" target="_blank">View details (PDF)</a></button>
             </div>
             <div class="pkgitem">
                 <img src="images/packages/pkg2.jpg" alt="pkg1" />
@@ -15,7 +17,7 @@
                 <br>
                 <p>5 days-6 nights<br><del>$2400 /-</del> $2100 /-</p>
                 <br>
-                <button>Plan</button>
+                <button><a href="sample.pdf" target="_blank">View details (PDF)</a></button>
             </div>
             <div class="pkgitem">
                 <img src="images/packages/pkg3.jpg" alt="pkg1" />
@@ -24,6 +26,7 @@
                 <br>
                 <p>5 days-6 nights<br><del>$3300 /-</del> $3000 /-</p>
                 <br>
-                <button>Plan</button>
+                <button><a href="sample.pdf" target="_blank">View details (PDF)</a></button>
             </div>
+</div>
 </section>
